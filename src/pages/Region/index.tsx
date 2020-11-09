fde085d27d79314cc3170db281e0222753a5f71a
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-11-09 13:35:49 -0300

    Adding Google Analytics

--- src/pages/Region/index.tsx
+++ src/pages/Region/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import ReactGA from 'react-ga';
 import Map from '../../components/MapRegion';
 
 const Region: React.FC = () => {
+  useEffect(() => {
+    ReactGA.initialize('UA-182410588-4');
+    ReactGA.pageview('/');
+  }, []);
   return <Map defaultYear={2018} defaultCategory="regional" />;
 };
 
